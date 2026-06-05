696004bf8ad2d3b621127193772db498b78743f9
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:36:49 -0500

    xml commenting

--- src/Soenneker.Firecrawl.Runners.OpenApiClient/Constants.cs
+++ src/Soenneker.Firecrawl.Runners.OpenApiClient/Constants.cs
@@ -1,6 +1,12 @@
 namespace Soenneker.Firecrawl.Runners.OpenApiClient;
 
+/// <summary>
+/// Represents the constants.
+/// </summary>
 public static class Constants
 {
+    /// <summary>
+    /// The library.
+    /// </summary>
     public const string Library = "Soenneker.Firecrawl.OpenApiClient";
 }
